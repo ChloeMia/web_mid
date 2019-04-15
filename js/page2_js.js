da04b7aa7fd2e25686a14ab6cdbb445afa0217e2
ChloeMia <48078968+ChloeMia@users.noreply.github.com>
2019-04-15 13:48:01 +0800

    page2 updata js

--- js/page2_js.js
+++ js/page2_js.js
@@ -3,6 +3,7 @@ var alltext = ["button1_t","button2_t","button3_t"];
 var hoverBefore = "rgb(234, 246, 247)";
 var hoverAfter = "rgb(131, 231, 240)";
 var clickAfter = "rgb(66, 211, 224)";
+
 function changeColorClick(id,id2)
 {
     for (var i = 0; i < allbutton.length; i++) {
@@ -15,10 +16,10 @@ function changeColorClick(id,id2)
     var y = document.getElementById(id2);
     x.style.backgroundColor = clickAfter;
     y.style.display = "block";
-
 }
+
 function changeColorOver(id)
-{
+{   
     for (var i = 0; i < allbutton.length; i++) {
         var d = document.getElementById(allbutton[i]);
         if(d.style.backgroundColor != clickAfter)
@@ -30,5 +31,4 @@ function changeColorOver(id)
         x.style.backgroundColor = hoverAfter;
     }
     
-
 }
